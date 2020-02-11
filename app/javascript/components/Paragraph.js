94f63c2cd8b590a1eb49d34f3e6da4011b4174c3
John Colagioia <john@colagioia.net>
2020-02-11 07:20:18 -0500

    Show replies (in a very primitive way)

--- app/javascript/components/Paragraph.js
+++ app/javascript/components/Paragraph.js
@@ -7,7 +7,7 @@ class Paragraph extends React.Component {
   constructor (props) {
     super(props);
   }
-  
+
   choose (text, idx, pid, pnumber, offset) {
     const punct = [
       '.',
@@ -47,7 +47,7 @@ class Paragraph extends React.Component {
       />);
     }
   }
-  
+
   paragraphParts (self, contentArray, pid, pnumber) {
     const components = [];
     const lengths = {
@@ -78,7 +78,35 @@ class Paragraph extends React.Component {
     });
     return components;
   }
-  
+
+  paragraphChildren(self, children, indent) {
+    const result = [];
+
+    if (
+      children === null ||
+      typeof children === 'undefined' ||
+      children.length === 0
+    ) {
+      return;
+    }
+
+    children.forEach(c => result.push(<Paragraph
+      avatar={c.avatar}
+      content={{
+        __html: c.content
+      }}
+      indent={indent + 1}
+      key={`child-${c.id}`}
+      pid={c.id}
+      pnum={c.id}
+      text={c.content}
+      ts={c.created_at.toString()}
+      when={c.created_at.toString()}
+      who={c.user_id.toString()}
+    />));
+    return result;
+  }
+
   render () {
     const array = JSON.parse(this.props.content.__html);
     const para = this.paragraphParts(
@@ -87,28 +115,47 @@ class Paragraph extends React.Component {
       this.props.pid,
       this.props.pnum
     );
+    const indent = this.props.indent === null
+      || typeof this.props.indent === 'undefined'
+      ? 0
+      : this.props.indent;
+    const children = this.paragraphChildren(
+      this,
+      this.props.children,
+      indent
+    );
+    const pad = `${indent}em`;
     return (
       <React.Fragment>
-        <div className="msg-paragraph">
-          <p
-            className="msg-par-text"
-          >
-            { para }
-          </p>
-          <p className="msg-par-avatar">
-            <a href='/'>
-              <img draggable="false" src={ this.props.avatar } />
-            </a>
-            <br />
-            <span className="id-time">{ this.props.who }</span>
-            <br />
-            <span
-              className="id-time"
-              title={ this.props.ts }
+        <div
+          style={{
+            marginLeft: pad
+          }}
+        >
+          <div className="msg-paragraph">
+            <p
+              className="msg-par-text"
             >
-              { this.props.when } ago
-            </span>
-          </p>
+              { para }
+            </p>
+            <p className="msg-par-avatar">
+              <a href='/'>
+                <img draggable="false" src={ this.props.avatar } />
+              </a>
+              <br />
+              <span className="id-time">{ this.props.who }</span>
+              <br />
+              <span
+                className="id-time"
+                title={ this.props.ts }
+              >
+                { this.props.when } ago
+              </span>
+            </p>
+          </div>
+          <div>
+            { children }
+          </div>
         </div>
       </React.Fragment>
     );
@@ -117,6 +164,7 @@ class Paragraph extends React.Component {
 
 Paragraph.propTypes = {
   avatar: PropTypes.string,
+  indent: PropTypes.number,
   pid: PropTypes.number,
   pnum: PropTypes.number,
   text: PropTypes.string,
